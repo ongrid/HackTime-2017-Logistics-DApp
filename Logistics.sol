a9fb03c4c45cf857a13692d88bc5b8adcd162e5b
achievement008 <achievement008@gmail.com>
2017-11-04 18:32:58 +0300

    Added another mandatory variables and mappings

--- Logistics.sol
+++ Logistics.sol
@@ -15,4 +15,20 @@ contract PostService {
         require(msg.sender == owner);
         _;
     }
+    
+    struct Item {
+        bytes32 name;
+        uint from;
+        uint to;
+        bytes32 weight;
+        bytes32 name_from;
+        bytes32 name_to;
+    }
+    
+    mapping (uint => bytes32) public stock_names;
+    mapping (uint => address) public stock_addresses;
+    
+    mapping (uint => Item) public items;
+    mapping (uint => uint[]) public item_paths;
+    mapping (uint => uint[]) public item_paths_time;
 } 
