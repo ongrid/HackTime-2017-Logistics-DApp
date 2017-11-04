f72369719812909721b1235bc673b28c1e052f16
achievement008 <achievement008@gmail.com>
2017-11-04 18:17:56 +0300

    Added basic constructor and variables

--- Logistics.sol
+++ Logistics.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.15;
 
 contract PostService {
-  
+    uint itemCount;
+    uint stockCount;
+    address owner;
+    
+    function PostService() public {
+        owner = msg.sender;
+        itemCount = 0;
+        stockCount = 0;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 } 
