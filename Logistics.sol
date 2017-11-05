fe896e83bb32680ab69017fb85ec539c1cb58430
achievement008 <achievement008@gmail.com>
2017-11-05 18:31:32 +0300

    Added all items and current item stock

--- Logistics.sol
+++ Logistics.sol
@@ -23,6 +23,7 @@ contract PostService {
         bytes32 weight;
         bytes32 name_from;
         bytes32 name_to;
+        uint current_stock;
     }
     
     mapping (uint => bytes32) public stock_names;
@@ -32,9 +33,17 @@ contract PostService {
     mapping (uint => uint[]) public item_paths;
     mapping (uint => uint[]) public item_paths_time;
     
+    uint[] public item_ids;
+    
     event NewItem(uint index);
     event NewStock(uint index);
-
+    
+    function get_all_items() public constant returns(uint[]){
+        return item_ids;
+    }
+    
+    /// "Клин","0xca35b7d915458ef540ade6068dfe2f44e8fa733c"
+    /// "Солнечногорск","0x14723a09acff6d2a60dcdf7aa4aff308fddc160c"
     function add_stock(bytes32 _name, address _address) public returns(uint) {
         stock_names[stockCount] = _name;
         stock_addresses[stockCount] = _address;
@@ -51,7 +60,9 @@ contract PostService {
     function get_stock_name(uint _index) constant public returns (bytes32) {
         return stock_names[_index];
     }
- 
+    
+    /// "Ботинок","0","1","1.2","Вася","Петя"
+    /// "Котлета","0","3","0.2","Петя","Вася"
     function add_item(bytes32 _name, uint _from, uint _to, bytes32 _weight, bytes32 _name_from, bytes32 _name_to) public returns(uint) {
         uint index = itemCount;
         items[index] = Item({
@@ -60,21 +71,24 @@ contract PostService {
             to: _to,
             weight: _weight,
             name_from: _name_from,
-            name_to: _name_to
+            name_to: _name_to,
+            current_stock: _from
         });
         item_paths[index].push(_from);
         item_paths_time[index].push(now);
         itemCount++;
         NewItem(index);
+        item_ids.push(index);
         return index;
     }
     
-    function get_item(uint _index) constant public returns (bytes32, uint, uint, bytes32, bytes32, bytes32){
-        return (items[_index].name, items[_index].from, items[_index].to, items[_index].weight, items[_index].name_from, items[_index].name_to);
+    function get_item(uint _index) constant public returns (bytes32, uint, uint, bytes32, bytes32, bytes32, uint){
+        return (items[_index].name, items[_index].from, items[_index].to, items[_index].weight, items[_index].name_from, items[_index].name_to, items[_index].current_stock);
     }
     
     function transfer(uint _index, uint _from, uint _to) public returns (bool) {
         require(item_paths[_index][item_paths[_index].length - 1] == _from && items[_index].to != _from && msg.sender == stock_addresses[_from]);
+        items[_index].current_stock = _to;
         item_paths[_index].push(_to);
         item_paths_time[_index].push(now);
         return true;
